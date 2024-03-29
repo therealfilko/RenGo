6ee6e8727620ed3e8b63ca7d75c97c75d761471e
TheRealFilko <fn.filipnikolic@gmail.com>
2024-03-29 23:24:12 +0100

    Added some stuff

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main()  {
+    // Für Filepath ist Eingabe ein String type und für flag, geht int, string und bool
+    // 1. Bereite die Flags vor
+    // 2. Erstelle Filepath Logik, muss schauen wie sowas geht 
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/therealfilko/RenGo
+
+go 1.21.1
